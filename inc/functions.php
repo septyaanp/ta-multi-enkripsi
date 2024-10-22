293578decab753c6af6a42532951aa9e823f54b5
septyaanp <septyaandiniputri@gmail.com>
2024-10-22 20:39:53 +0700

    update function.php

--- inc/functions.php
+++ inc/functions.php
@@ -2,52 +2,56 @@
 require_once 'config.php';
 
 // Fungsi Diffie-Hellman
-function diffie_hellman($prime, $generator, $private_key) {
-    return bcpowmod($generator, $private_key, $prime);
+function generateDHKeys() {
+    $config = [
+        "private_key_bits" => 2048,
+        "private_key_type" => OPENSSL_KEYTYPE_DH,
+    ];
+    
+    $res = openssl_pkey_new($config);
+    openssl_pkey_export($res, $privKey);
+    $pubKey = openssl_pkey_get_details($res)['dh']['pub_key'];
+    
+    return ['private' => $privKey, 'public' => $pubKey];
+}
+
+function computeSharedSecret($privKey, $otherPubKey) {
+    $privKeyResource = openssl_pkey_get_private($privKey);
+    return openssl_dh_compute_key($otherPubKey, $privKeyResource);
 }
 
 // Fungsi Blowfish
-function blowfish_encrypt($data, $key) {
-    $cipher = "bf-cbc";
-    $ivlen = openssl_cipher_iv_length($cipher);
-    $iv = openssl_random_pseudo_bytes($ivlen);
-    $encrypted = openssl_encrypt($data, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+function blowfishEncrypt($data, $key) {
+    $iv = openssl_random_pseudo_bytes(8);
+    $encrypted = openssl_encrypt($data, 'BF-CBC', $key, OPENSSL_RAW_DATA, $iv);
     return base64_encode($iv . $encrypted);
 }
 
-function blowfish_decrypt($data, $key) {
-    $cipher = "bf-cbc";
-    $ivlen = openssl_cipher_iv_length($cipher);
-    $data = base64_decode($data);
-    $iv = substr($data, 0, $ivlen);
-    $encrypted = substr($data, $ivlen);
-    return openssl_decrypt($encrypted, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+function blowfishDecrypt($encryptedData, $key) {
+    $data = base64_decode($encryptedData);
+    $iv = substr($data, 0, 8);
+    $encrypted = substr($data, 8);
+    return openssl_decrypt($encrypted, 'BF-CBC', $key, OPENSSL_RAW_DATA, $iv);
 }
 
 // Fungsi untuk menambah data
-function add_data($name, $username, $password, $email, $phone) {
+function add_data($name, $email, $phone) {
     global $conn;
     
-    $blowfish_key = substr(bin2hex(random_bytes(56)), 0, 56);
-    $encrypted_phone = blowfish_encrypt($phone, $blowfish_key);
+    $keys = generateDHKeys();
+    $public_key = base64_encode($keys['public']);
+    $private_key = base64_encode($keys['private']);
     
-    // Hash password sebelum disimpan
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $sessionKey = hash('sha256', $keys['private'], true);
+    $encrypted_phone = blowfishEncrypt($phone, $sessionKey);
     
-    $sql = "INSERT INTO users (name, username, password, email, phone, blowfish_key) VALUES (?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO users (name, email, phone, public_key, private_key) VALUES (?, ?, ?, ?, ?)";
     $stmt = mysqli_prepare($conn, $sql);
-    if (!$stmt) {
-        return "Error preparing statement: " . mysqli_error($conn);
-    }
+    mysqli_stmt_bind_param($stmt, "sssss", $name, $email, $encrypted_phone, $public_key, $private_key);
     
-    mysqli_stmt_bind_param($stmt, "ssssss", $name, $username, $hashed_password, $email, $encrypted_phone, $blowfish_key);
-    
-    if(mysqli_stmt_execute($stmt)){
-        return true;
-    } else {
-        return "Error executing statement: " . mysqli_stmt_error($stmt);
-    }
+    return mysqli_stmt_execute($stmt);
 }
+
 // Fungsi untuk mengambil data
 function get_data() {
     global $conn;
@@ -57,31 +61,26 @@ function get_data() {
 }
 
 // Fungsi untuk mengedit data
+// Lanjutan fungsi edit_data
 function edit_data($id, $name, $email, $phone) {
     global $conn;
 
-    // Ambil public key dari database
-    $sql = "SELECT public_key FROM users WHERE id = ?";
+    $sql = "SELECT private_key FROM users WHERE id = ?";
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, "i", $id);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     $row = mysqli_fetch_assoc($result);
-    $public_key = $row['public_key'];
+    $private_key = base64_decode($row['private_key']);
 
-    // Enkripsi data menggunakan Blowfish
-    $blowfish_key = $public_key; // Gunakan public key sebagai kunci Blowfish
-    $encrypted_phone = blowfish_encrypt($phone, $blowfish_key);
+    $sessionKey = hash('sha256', $private_key, true);
+    $encrypted_phone = blowfishEncrypt($phone, $sessionKey);
 
     $sql = "UPDATE users SET name = ?, email = ?, phone = ? WHERE id = ?";
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, "sssi", $name, $email, $encrypted_phone, $id);
 
-    if(mysqli_stmt_execute($stmt)){
-        return true;
-    } else {
-        return false;
-    }
+    return mysqli_stmt_execute($stmt);
 }
 
 // Fungsi untuk menghapus data
@@ -92,4 +91,47 @@ function delete_data($id) {
     mysqli_stmt_bind_param($stmt, "i", $id);
     return mysqli_stmt_execute($stmt);
 }
-?>
+
+// Fungsi untuk mendekripsi nomor telepon
+function decrypt_phone($encrypted_phone, $private_key) {
+    $private_key = base64_decode($private_key);
+    $sessionKey = hash('sha256', $private_key, true);
+    return blowfishDecrypt($encrypted_phone, $sessionKey);
+}
+
+// Fungsi untuk login
+function login($username, $password) {
+    global $conn;
+    $sql = "SELECT id, password FROM users WHERE username = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $username);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    
+    if ($row = mysqli_fetch_assoc($result)) {
+        if (password_verify($password, $row['password'])) {
+            return $row['id'];
+        }
+    }
+    return false;
+}
+
+// Fungsi untuk registrasi
+function register($username, $password, $name, $email, $phone) {
+    global $conn;
+    
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    
+    $keys = generateDHKeys();
+    $public_key = base64_encode($keys['public']);
+    $private_key = base64_encode($keys['private']);
+    
+    $sessionKey = hash('sha256', $keys['private'], true);
+    $encrypted_phone = blowfishEncrypt($phone, $sessionKey);
+    
+    $sql = "INSERT INTO users (username, password, name, email, phone, public_key, private_key) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "sssssss", $username, $hashed_password, $name, $email, $encrypted_phone, $public_key, $private_key);
+    
+    return mysqli_stmt_execute($stmt);
+}
